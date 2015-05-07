bf4f43d54d610df4ff0936a3dbf0a221ac1f2a92
kylavandenbogaerde <k.m.vandenbogaerde@students.uu.nl>
2015-05-07 15:51:53 +0200

    opdracht3

---
+++ Opdracht3/estimate_pi.py
@@ -0,0 +1,38 @@
+import random
+import math
+import sys
+
+
+d=1                           #afstand tussen de lijnen
+L = int(sys.argv[2])          #lengte van de naald
+if L>d:
+    print('L should be smaller than d')
+    exit()
+N=int(sys.argv[1])            #aantal herhalingen experiment
+
+def drop_needle(l):
+    x=random.random()
+    a=random.vonmisesvariate(0,0)
+    if abs(L*math.cos(a))>abs(d-x):
+        return (True)
+    else:
+        return(False)
+juist=0
+
+for i in range(N):
+    if drop_needle(L):
+        juist+=1            #juist=juist+1
+
+print(str(juist) + ' hits in ' + str(N) + ' tries')
+
+Pi= 2*L*N / juist
+print('Pi = '+str(Pi))
+
+    
+
+
+
+    
+
+    
+
