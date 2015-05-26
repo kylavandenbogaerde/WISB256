3f23006df04eee0350f2bfa987a25ebdcdc439a5
kylavandenbogaerde <k.m.vandenbogaerde@students.uu.nl>
2015-05-26 10:43:48 +0200

    versie

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -3,6 +3,10 @@ import math
 lijst=[]
 
 def findRoot(f, a, b, epsilon):
+    if f(a)==0:
+        return a
+    if f(b)==0:
+        return b
     if abs(b-a)>epsilon:
         #print(a,b)
         m=(a+b)/2
@@ -12,25 +16,31 @@ def findRoot(f, a, b, epsilon):
 
         if f(a)>0 and f(m)<0:
             #print([a,m])
-            findRoot(f,a,m,epsilon)
-            return
+            return findRoot(f,a,m,epsilon)
+            
 
         elif f(a)<0 and f(m)>0:
             #print([a,m])
-            findRoot(f,a,m,epsilon)
-            return
+            return findRoot(f,a,m,epsilon)
+            
 
         elif f(b)>0 and f(m)<0:
             #print([m,b])
-            findRoot(f,m,b,epsilon)
-            return
+            return findRoot(f,m,b,epsilon)
+            
 
         elif f(b)<0 and f(m)>0:
             #print([m,b])
-            findRoot(f,m,b,epsilon)
-            return
+            return findRoot(f,m,b,epsilon)
+            
     else:
-        print(lijst[-1])
+        m=(a+b)/2
+        return m
+
+#def f(x):
+ #   return x-1
+
+        
         
 
 
