9f782317736d15c0c209d9fc1f58d9de74017e73
kylavandenbogaerde <k.m.vandenbogaerde@students.uu.nl>
2015-05-28 16:47:19 +0200

    bonus2

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -42,38 +42,51 @@ def findRoot(f, a, b, epsilon):
     
 #root = findRoot(lambda x:x - 1,0,3,.1)
 
-def findALLRoots(f, a, b, epsilon):
+
+
+
+def findALLRoots(f,a,b,epsilon):
     nulpunten=[]
-    if f(a)==0:
-        nulpunten.append(a)
-    if f(b)==0:
-        nulpunten.append(b)
-    if abs(b-a)>epsilon:
+    aantal_intervallen= int((b-a)/epsilon)
+    #print(aantal_intervallen)
+    for i in range(aantal_intervallen):
+        if f(a+(i+1)*epsilon)*f(a+i*epsilon)<=0:
+            wortel=findRoot(f,a+i*epsilon, a+(i+1)*epsilon, epsilon)
+            nulpunten=nulpunten+[wortel]
+            i=i+1
+            #print(nulpunten)
+    return (list(set(nulpunten)))
+
+#def findALLRoots(f, a, b, epsilon):
+#    nulpunten=[]
+#    if f(a)==0:
+#        nulpunten.append(a)
+#    if f(b)==0:
+#        nulpunten.append(b)
+#    if abs(b-a)>epsilon:
         #print(a,b)
-        m=(a+b)/2
+#        m=(a+b)/2
         #print(m)
         #print(f(m))
-        lijst.append(m)
-        if f(m)==0:
-            nulpunten.append(m)
+#        lijst.append(m)
+#        if f(m)==0:
+#            nulpunten.append(m)
 
-        if f(a)*f(m)<0:
+#        if f(a)*f(m)<0:
             #print([a,m])
-            findALLRoots(f,a,m,epsilon)
+#            findALLRoots(f,a,m,epsilon)
             
-        if f(b)*f(m)<0:
+#        if f(b)*f(m)<0:
             #print([m,b])
-            findALLRoots(f,m,b,epsilon)
+#            findALLRoots(f,m,b,epsilon)
            
-    else:
-        m=(a+b)/2
-        nulpunten.append(m)
+#    else:
+#        m=(a+b)/2
+#        nulpunten.append(m)
         #findALLRoots(f,a,m-epsilon,epsilon)
        # findALLRoots(f, m+epsilon, b, epsilon)
-    print(nulpunten)
+#    print(nulpunten)
        # findALLRoots(f, a, b, epsilon)
-
-
         
     
     
