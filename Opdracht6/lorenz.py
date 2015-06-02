4bc90dcfa0ad5c8d2c19b4dc3fac1ea576d170de
kylavandenbogaerde <k.m.vandenbogaerde@students.uu.nl>
2015-06-02 12:22:42 +0200

    opdracht6

--- Opdracht6/lorenz.py
+++ Opdracht6/lorenz.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D
 
-sigma = 0.0
-
 class Lorenz:
+
     def __init__(self, begincoordinaten, sigma_ =10., rho_ =28., beta_ =8./3.):
         self.sigma=sigma_
         self.rho=rho_
@@ -28,23 +27,4 @@ class Lorenz:
         return a
 
 
-L1=Lorenz([-1,1,0])
-ant1=L1.solve(50, 0.01)
-print(ant1)
 
-# v = beginpunt
-# for alle tijdstippen:
-#   v += dv(t, x(-1), y(-1), z(-1)/dt * (deze - vorige) # dv/dt = [ dx/dt, dy/dt, dz/dt ]
-
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-ax.plot(ant1[:,0],ant1[:,1],ant1[:,2])
-ax.set_xlabel("X Axis")
-ax.set_ylabel("Y Axis")
-ax.set_zlabel("Z Axis")
-ax.set_title("Lorenz Attractor")
-
-plt.show()    
